48bd1a6c85eee449953e39801bf3ae1aea7569cb
alirezaRanjbar4 <152643636+alirezaRanjbar4@users.noreply.github.com>
2024-07-01 17:41:26 +0330

    Add files via upload
    init

---
+++ NumberToPersianWords/NumberToPersianWords/NumberToPersianWords.cs
@@ -0,0 +1,80 @@
+﻿namespace NumberToPersianWords
+{
+    public class NumberToPersianWords
+    {
+        private static readonly string[] Ones = {
+        "صفر", "یک", "دو", "سه", "چهار", "پنج", "شش", "هفت", "هشت", "نه",
+        "ده", "یازده", "دوازده", "سیزده", "چهارده", "پانزده", "شانزده", "هفده", "هجده", "نوزده"
+    };
+
+        private static readonly string[] Tens = {
+        "", "", "بیست", "سی", "چهل", "پنجاه", "شصت", "هفتاد", "هشتاد", "نود"
+    };
+
+        private static readonly string[] Hundreds = {
+        "", "صد", "دویست", "سیصد", "چهارصد", "پانصد", "ششصد", "هفتصد", "هشتصد", "نهصد"
+    };
+
+        private static readonly Dictionary<long, string> PowersOfThousand = new Dictionary<long, string>
+    {
+        { 1000000000000, "تریلیون" },
+        { 1000000000, "میلیارد" },
+        { 1000000, "میلیون" },
+        { 1000, "هزار" },
+        // اضافه کردن واحدهای بزرگتر در صورت نیاز
+    };
+
+        public static string Convert(long number)
+        {
+            if (number == 0)
+                return Ones[0];
+
+            return ConvertToWords(number);
+        }
+
+        private static string ConvertToWords(long number)
+        {
+            if (number < 0)
+                return "منفی " + ConvertToWords(Math.Abs(number));
+
+            string words = "";
+
+            if (number < 20)
+            {
+                words = Ones[number];
+            }
+            else if (number < 100)
+            {
+                words = Tens[number / 10];
+                if ((number % 10) > 0)
+                    words += " و " + Ones[number % 10];
+            }
+            else if (number < 1000)
+            {
+                words = Hundreds[number / 100];
+                if ((number % 100) > 0)
+                    words += " و " + ConvertToWords(number % 100);
+            }
+            else
+            {
+                foreach (var power in PowersOfThousand)
+                {
+                    if (number >= power.Key)
+                    {
+                        long baseValue = power.Key;
+                        long numBaseUnits = number / baseValue;
+                        long remainder = number % baseValue;
+
+                        words = ConvertToWords(numBaseUnits) + " " + power.Value;
+                        if (remainder > 0)
+                            words += " و " + ConvertToWords(remainder);
+
+                        break;
+                    }
+                }
+            }
+
+            return words;
+        }
+    }
+}
---
+++ NumberToPersianWords/NumberToPersianWords/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        long number = 700;
+        string result = NumberToPersianWords.NumberToPersianWords.Convert(number);
+
+        long number1 = 999999;
+        string result1 = NumberToPersianWords.NumberToPersianWords.Convert(number1);
+
+        long number2 = 0;
+        string result2 = NumberToPersianWords.NumberToPersianWords.Convert(number2);
+
+        long number3 = 387;
+        string result3 = NumberToPersianWords.NumberToPersianWords.Convert(number3);
+
+        long number4 = 2;
+        string result4 = NumberToPersianWords.NumberToPersianWords.Convert(number4);
+
+        long number5 = 70000000000;
+        string result5 = NumberToPersianWords.NumberToPersianWords.Convert(number5);
+
+        long number6 = 14;
+        string result6 = NumberToPersianWords.NumberToPersianWords.Convert(number6);
+
+        long number7 = 0005;
+        string result7 = NumberToPersianWords.NumberToPersianWords.Convert(number7);
+
+
+        Console.WriteLine(result);
+    }
+}
